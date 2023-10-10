49c7ef0761aeef3f5dbb8aa9c4a177ace7735132
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-10-10 16:06:46 +0300

    Add files via upload

---
+++ odev1550/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace odev1550
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCift = new System.Windows.Forms.ListBox();
+            this.lbTek = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbCift
+            // 
+            this.lbCift.FormattingEnabled = true;
+            this.lbCift.Location = new System.Drawing.Point(94, 108);
+            this.lbCift.Name = "lbCift";
+            this.lbCift.Size = new System.Drawing.Size(120, 316);
+            this.lbCift.TabIndex = 0;
+            // 
+            // lbTek
+            // 
+            this.lbTek.FormattingEnabled = true;
+            this.lbTek.Location = new System.Drawing.Point(248, 108);
+            this.lbTek.Name = "lbTek";
+            this.lbTek.Size = new System.Drawing.Size(120, 316);
+            this.lbTek.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "çift sayılar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "tek sayılar";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(248, 25);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 20);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(105, 25);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTek);
+            this.Controls.Add(this.lbCift);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbCift;
+        private System.Windows.Forms.ListBox lbTek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
---
+++ odev1550/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev1550
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            {
+
+                int sayi = Convert.ToInt32(txtSayi.Text);
+                if (sayi % 2 == 0)
+                {
+                    lbCift.Items.Add(sayi);
+                }
+                else
+                {
+                    lbTek.Items.Add(sayi);
+                }
+            }
+        }
+    }
+}
